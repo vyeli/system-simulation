b40081e236d6e5631b6498e9d3cd49dcd9a12d61
vyeli <yeliv9527@gmail.com>
2023-03-11 20:12:23 -0300

    add python script to generate random input file

---
+++ TP1/utils/generateInput.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+# Usage example: py generateInput.py 100 20
+
+N = int(sys.argv[1])
+L = int(sys.argv[2])
+
+file = open("static.txt", "w")
+file.write(str(N) + '\n' + str(L) + '\n');
+
+for i in range(N):
+    file.write(str(random.uniform(0, 1)) + '\n')
+    
+
+
