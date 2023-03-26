17394236f23588d9d46efeaadf117548d25f4761
VALENTIN YE LI <yeliv9527@gmail.com>
2023-03-26 16:50:03 -0300

    fix: serialize now only write the living cells

--- TP2/src/main/java/helpers/Serializer.java
+++ TP2/src/main/java/helpers/Serializer.java
@@ -3,10 +3,8 @@ package helpers;
 import java.util.Set;
 
 public class Serializer {
-    public static String serialize2D(Set<Pair<Integer, Integer>> liveCells, int size, int domain) {
+    public static String serialize2D(Set<Pair<Integer, Integer>> liveCells) {
         StringBuilder sb = new StringBuilder();
-        sb.append(size).append("\n");
-        sb.append(domain).append("\n");
         for (Pair<Integer, Integer> cell : liveCells) {
             sb.append(cell.getX());
             sb.append(" ");
@@ -16,10 +14,8 @@ public class Serializer {
         return sb.toString();
     }
 
-    public static String serialize3D(Set<Trio<Integer, Integer, Integer>> liveCells, int size, int domain) {
+    public static String serialize3D(Set<Trio<Integer, Integer, Integer>> liveCells) {
         StringBuilder sb = new StringBuilder();
-        sb.append(size).append("\n");
-        sb.append(domain).append("\n");
         for (Trio<Integer, Integer, Integer> cell : liveCells) {
             sb.append(cell.getX());
             sb.append(" ");
