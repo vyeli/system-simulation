1857b029a6536f1cd7a7e9407cafe7fb2e0e5601
Fegrant <nicolasbirsa333@gmail.com>
2023-04-21 11:45:18 -0300

    chore: changed hist graph to pdf

--- TP3/graphs/event_histogram.py
+++ TP3/graphs/event_histogram.py
@@ -2,22 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-events = pd.read_csv('./execution_data.csv')
+y0_plots = [42.0, 48.22222222222222, 56.0]
 
 fig, ax = plt.subplots()
 
-bins = [0, 0.05, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 20]
+df = pd.read_csv('./execution_data.csv')
 
-plt.hist(events['event_time'], bins=bins)
+for y0, grouped_by_y0 in df.groupby('y0'):
+    print(y0)
+    if y0 in y0_plots:
+        event_times = grouped_by_y0['event_time']
 
-print(events['event_time'])
+        hist, bin_egdes = np.histogram(event_times, bins='scott')
+        bins = len(hist)
+
+        y = []
+        for i in range(bins):
+            y.append(hist[i] / ((bin_egdes[i+1] - bin_egdes[i]) * bins))
+
+        plt.loglog(y, label=round(y0, 3))
+
+plt.legend()
+#data = df['event_time']
+
+#bin_width = 3.5 * np.std(data) / np.power(len(data), 1/3)
+#bins = int((np.max(data) - np.min(data)) / bin_width)
+
+# plt.hist(events['event_time'], bins='scott')
 
 # plt.stairs(counts, bins)
 
 # ax.plot(bins, events['event_time'], '--')
 
-plt.xlabel('Tiempo entre eventos')
-plt.ylabel('Cantidad de eventos')
+plt.xlabel('Tiempo entre eventos [s]')
+plt.ylabel('Densidad de probabilidad')
+
+#plt.xlim(0, 50)
+#plt.ylim(0, 40000)
 
 # fig.tight_layout()
 plt.show()
